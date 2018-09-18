df97a82147063f1a3de86bdb7ab04d324efb6230
Pawel <pawoscil@gmail.com>
2018-09-18 10:10:15 +0200

    Add project structure

---
+++ src/main/java/pl/sporttown/SportTownApplication.java
@@ -0,0 +1,12 @@
+package pl.sporttown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportTownApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SportTownApplication.class, args);
+    }
+}
