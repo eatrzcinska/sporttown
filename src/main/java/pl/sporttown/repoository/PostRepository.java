313d57634c15680b63150bdd44fb5e74cd1ef096
GosiaBakM <machmalgorzata@gmail.com>
2018-09-19 10:41:43 +0200

    Add post unctionality
    -post controller methods added ( add post, delete post, search by date)

--- src/main/java/pl/sporttown/repoository/PostRepository.java
+++ src/main/java/pl/sporttown/repoository/PostRepository.java
@@ -1,11 +1,15 @@
 package pl.sporttown.repoository;
 
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import pl.sporttown.model.Post;
 
-@Repository
-public interface PostRepository extends CrudRepository<Post,Long> {
+import java.util.Date;
+import java.util.Optional;
 
+@Repository
+public interface PostRepository extends JpaRepository<Post,Long> {
+
+    Optional<Post> findAllByData(Date date);
 }
