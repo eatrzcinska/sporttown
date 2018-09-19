8c8d966abfc37e1048f849e0f79d7e46caa70cb5
GosiaBakM <machmalgorzata@gmail.com>
2018-09-19 11:02:03 +0200

    Add post unctionality
    -post controller methods added ( add post, delete post, search by date)

--- src/main/java/pl/sporttown/controller/PostController.java
+++ src/main/java/pl/sporttown/controller/PostController.java
@@ -1,9 +1,16 @@
 package pl.sporttown.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import pl.sporttown.model.Post;
 import pl.sporttown.service.PostService;
 
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
+@RequestMapping(path = "/post/")
 @Controller
 public class PostController {
 
@@ -12,4 +19,55 @@ public class PostController {
     public PostController(PostService service) {
         this.service = service;
     }
+
+    @GetMapping(path = "/add")
+    public String add(Model model) {
+        Post post = new Post();
+        model.addAttribute("post", post);
+        return "postAdd";
+    }
+
+    @PostMapping(path = "/add")
+    public String createPost(Post post){
+        service.create(post);
+        return "redirect:/post/list";
+    }
+
+    @GetMapping(path = "/list")
+    public String postList(Model model){
+        List <Post> postList = service.findAll();
+        model.addAttribute("postList", postList);
+        return "postList";
+    }
+
+    //Do zmiany
+    @GetMapping(path = "/find/{data}")
+    public String findPostByDate(@PathVariable(name = "data")Date data, Model model){
+        Optional<Post> postsByData = service.findByDate(data);
+
+        if (postsByData.isPresent()) {
+            model.addAttribute("post", postsByData.get());
+            return "redirect:/post/list";
+        }
+        return "redirect:/error";
+    }
+
+    //Do zmiany
+    @GetMapping(path = "/find/{id}")
+    public String findPostById(@PathVariable(name = "id") Long id, Model model){
+            Optional<Post> postsById = service.findById(id);
+
+            if (postsById.isPresent()) {
+                model.addAttribute("post", postsById.get());
+                return "postDetails";
+            }
+            return "redirect:/error";
+        }
+
+    @DeleteMapping(path = "/remove/{id}")
+    public String removePost(@PathVariable(name = "id")Long id){
+        service.removePost(id);
+        return "redirect:/post/list";
+    }
+
 }
--- src/main/java/pl/sporttown/service/PostService.java
+++ src/main/java/pl/sporttown/service/PostService.java
@@ -2,7 +2,12 @@ package pl.sporttown.service;
 
 
 import org.springframework.stereotype.Service;
+import pl.sporttown.model.Post;
 import pl.sporttown.repoository.PostRepository;
+
+import java.util.Date;
+import java.util.List;
+import java.util.Optional;
 
 @Service
 public class PostService {
@@ -12,4 +17,26 @@ public class PostService {
     public PostService(PostRepository repository) {
         this.repository = repository;
     }
+
+    public void create(Post post){
+        repository.save(post);
+    }
+
+    public List<Post> findAll(){
+        return repository.findAll();
+    }
+
+    public Optional<Post> findByDate(Date data) {
+        return repository.findAllByData(data);
+    }
+
+    public Optional<Post> findById(Long id) {
+        return repository.findById(id);
+    }
+
+    public void removePost(Long id){
+        repository.deleteById(id);
+    }
+
+
 }
